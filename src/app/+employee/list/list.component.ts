a1bbef0b1e5a1e4da4eaec8bdc6d734750658cce
ToBePlus <float1101@gmail.com>
2016-09-08 16:00:00 +0800

    无限滚动

--- src/app/+employee/list/list.component.ts
+++ src/app/+employee/list/list.component.ts
@@ -4,74 +4,119 @@ import { EmployeeApi } from 'client';
 
 
 @Component({
-    selector: 'employee-list',
-    template: require('./list.template.html'),
-    styles: [require('./list.style.scss')],
-    directives: [...ROUTER_DIRECTIVES],
-    providers: [EmployeeApi]
+  selector: 'employee-list',
+  template: require('./list.template.html'),
+  styles: [require('./list.style.scss')],
+  directives: [...ROUTER_DIRECTIVES],
+  providers: [EmployeeApi]
 })
 export class EmployeeList implements OnInit {
-    page: any = {};
-    employees: any[] = [];
-    showDelWin: boolean = false;
-    delEmployee: any;
-    constructor(private eApi: EmployeeApi, private router: Router, private route: ActivatedRoute) {
-        this.page.current = String(1);
-        this.page.limit = String(20);
-    }
+  page: any = {};
+  employees: any[] = [];
+  showDelWin: boolean = false;
+  delEmployee: any;
+  //滚动相关
+  timeout: any;
+  next: boolean = false;
+  loading: boolean = false;
+  end: boolean = false;
+  isReturnTop: boolean = false;
+  returnTop: boolean = false;
 
-    ngOnInit() {
-        this.getEmployeeList(this.page.current, this.page.limit);
-    }
+  constructor(private eApi: EmployeeApi, private router: Router, private route: ActivatedRoute) {
+    this.page.current = String(1);
+    this.page.limit = String(20);
+  }
 
-    getEmployeeList(curPage, pageSize) {
-        this.eApi.employeeListGet(curPage, pageSize ).subscribe(data => {
-            if (data.meta&&data.meta.code === 200 && data.data) {
-                this.employees = data.data.length ? data.data : [];
-            } else {
-                if (data.error && data.error.message) {
-                    console.log(data.error.message);
-                }
-            }
-        }, err => {
-            console.error(err);
-            this.employees = [];
-        });
-    }
+  ngOnInit() {
+    this.getEmployeeList(this.page.current, this.page.limit);
+  }
 
-    
-    onEditEmployee(employee, e) {
-        e.stopPropagation();
-        this.router.navigate(['/dashboard/employee/edit', employee.id]);
+  getEmployeeList(curPage, pageSize, scroll = false) {
+    this.loading = true;
+
+    window.clearTimeout(this.timeout);
+    this.timeout = window.setTimeout(() => {
+      if (scroll && !this.end) {
+        this.page.current += 1;
+      }
+      this.eApi.employeeListGet(curPage, pageSize).subscribe(res => {
+        if (res.meta && res.meta.code === 200 && res.data) {
+          if (scroll) {
+            this.employees = this.employees.concat(res.data);
+          } else {
+            this.employees = res.data;
+          }
+        } else {
+          this.employees = [];
+          this.end = true;
+          alert(res.error.message);
+        }
+        this.loading = false;
+        // if (data.meta && data.meta.code === 200 && data.data) {
+        //   this.employees = data.data.length ? data.data : [];
+        // } else {
+        //   if (data.error && data.error.message) {
+        //     console.log(data.error.message);
+        //   }
+        // }
+      }, err => {
+        console.error(err);
+        this.employees = [];
+      });
+    }, 500);
+  }
+
+
+  onEditEmployee(employee, e) {
+    e.stopPropagation();
+    this.router.navigate(['/dashboard/employee/edit', employee.id]);
+  }
+  onAddNewEmployee() {
+    this.router.navigate(['/dashboard/employee/add']);
+  }
+
+  //无限滚动
+  onScrollEnd(next) {
+    this.next = next;
+    if (next && !this.loading) {
+      this.getEmployeeList(this.page.current, this.page.limit, true);
     }
-    onAddNewEmployee() {
-        this.router.navigate(['/dashboard/employee/add']);
-    }
-    // onDelEmployee() {
-    //     this.capi.customerCustomerIdDeleteDelete(String(this.delCustomer.id)).subscribe( data => {
-    //         if (data.meta&&data.meta.code === 200) {
-    //             this.onCloseDelWin();
-    //             this.getCustomerList(this.page.current, this.page.limit);
-    //         } else {
-    //             if (data.error && data.error.message) {
-    //                 console.log(data.error.message);
-    //             }
-    //         }
-    //     }, err => {
-    //         console.error(err);
-    //     });
-    // }
-    // onCloseDelWin() {
-    //     this.showDelWin = false;
-    //     this.delEmployee = undefined;
-    // }
-    // onShowDelWin(employee, e) {
-    //     e.stopPropagation();
-    //     this.showDelWin = true;
-    //     this.delEmployee = employee;
-    // }
-    // onViewCustomerDetail(customer, e) {
-    //     e.stopPropagation();
-    //     this.router.navigate(['/dashboard/customer/detail', customer.id]);
-    // }
+  }
+  onScrollTop(returnTop) {
+    this.isReturnTop = !returnTop;
+    this.returnTop = !!returnTop;
+  }
+  //返回头部
+  onReturnTop() {
+    this.returnTop = true;
+  }
+
+  // onDelEmployee() {
+  //     this.capi.customerCustomerIdDeleteDelete(String(this.delCustomer.id)).subscribe( data => {
+  //         if (data.meta&&data.meta.code === 200) {
+  //             this.onCloseDelWin();
+  //             this.getCustomerList(this.page.current, this.page.limit);
+  //         } else {
+  //             if (data.error && data.error.message) {
+  //                 console.log(data.error.message);
+  //             }
+  //         }
+  //     }, err => {
+  //         console.error(err);
+  //     });
+  // }
+  // onCloseDelWin() {
+  //     this.showDelWin = false;
+  //     this.delEmployee = undefined;
+  // }
+  // onShowDelWin(employee, e) {
+  //     e.stopPropagation();
+  //     this.showDelWin = true;
+  //     this.delEmployee = employee;
+  // }
+  // onViewCustomerDetail(customer, e) {
+  //     e.stopPropagation();
+  //     this.router.navigate(['/dashboard/customer/detail', customer.id]);
+  // }
 }
