dd85e3b3ad05cdea8c8002d989ac2dc7697d98aa
ToBePlus <float1101@gmail.com>
2016-09-09 18:23:59 +0800

    bug fixed

---
+++ src/app/+customer/detail/index.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { CustomerDetail } from './detail.component';
+import { BusinessTab } from '../../business-tab/business-tab';
+import { BTModule } from '../../business-tab/bt.module';
+
+
+// async components must be named routes for WebpackAsyncRoute
+export const routes = [
+  { path: '', component: CustomerDetail }
+];
+
+@NgModule({
+  declarations: [
+    // Components / Directives/ Pipes
+    CustomerDetail,
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(routes),
+    BTModule,
+  ]
+})
+export default class CustomerDetailModule {
+  static routes = routes;
+}
